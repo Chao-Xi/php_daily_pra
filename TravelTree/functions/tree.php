011ee71745c31184e5dcea62fa5b902cec04e01e
Chao XI <xichao2014@gmail.com>
2017-02-03 00:29:12 -0500

    php array tree traveling skill

---
+++ TravelTree/functions/tree.php
@@ -0,0 +1,13 @@
+<?php
+    function treeOut($tree)
+    {
+       $markup='';
+       foreach($tree as $branch=>$twig)
+       {
+       	  $markup.='<li>'.(is_array($twig) ? $branch.treeOut($twig) : $twig).'</li>';
+       }
+       return '<ul>'.$markup.'</ul>';	
+    }
+    //1. is_array()
+    //2. call the function itself
+    
